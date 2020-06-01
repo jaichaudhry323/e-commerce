db4268cf64e2f2d06f4294859dc1155b0bf14492
jaichaudhry323 <jaichaudhry323@gmail.com>
2020-06-01 19:30:35 +0530

    finished

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,14 +3,19 @@ import './menu-item.styles.scss'
 
 const MenuItem = ({ title, imageUrl, size }) => (
 
+    // if we keep the image in below one div then on enlargement of image size of div will increase leading to overlap with other div's (menu-items)
+    // so we put the image in another div so that the div containing image, size of this increases thus solving our problem 
     <div
-        style={{
-            backgroundImage: `url(${imageUrl})`     // image added dynamically
-        }}
-        
         // className='menu-item'                   // static class name
         className={`${size} menu-item`}             // dynamic class name 
     >
+
+        <div
+        className='background-image'
+        style={{
+            backgroundImage: `url(${imageUrl})`     // image added dynamically
+        }}
+        />
 
         <div className='content'>
 
