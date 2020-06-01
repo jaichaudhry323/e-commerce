0b3d6afe8484a1cac6f17e20b646561c501ae070
jaichaudhry323 <jaichaudhry323@gmail.com>
2020-06-02 02:05:23 +0530

    styling and firebase pending

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import HomePage from './pages/homepage/homepage.component';
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component';
 
 // writing     exact == get of node express server 
 // not writing exact == .use of node expresss server
@@ -20,6 +21,7 @@ function App() {
     <Switch>
       <Route exact path='/' component={HomePage} />
       <Route path='/shop' component={ShopPage} />
+      <Route path='/signin' component={SignInAndSignUpPage}/>
       </Switch>
 
     </div>
