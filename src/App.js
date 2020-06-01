6822309ced0d8360879df12d24c4f84932d39f7b
jaichaudhry323 <jaichaudhry323@gmail.com>
2020-06-01 21:08:48 +0530

    All changes in App.js

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import { Route, Link } from 'react-router-dom';
 
-const TopicsList = () => {
+// DYNAMIC LINKS
+// RENDERING A BUNCH OF TOPICS TO A BUNCH OF LINKS
+const TopicsList = (props) => {
   return (
     <div>
-      <h1>
-        TOPIC LIST PAGE
-      </h1>
+      <h1>TOPIC LIST PAGE</h1>
+
+      <Link to={`${props.match.url}/13`}>TO TOPIC 13</Link> 
+      &nbsp;&nbsp;&nbsp;
+      <Link to={`${props.match.url}/17`}>TO TOPIC 17</Link>
+      &nbsp;&nbsp;&nbsp;
+      <Link to={`${props.match.url}/21`}>TO TOPIC 21</Link>
+
     </div>
   )
 }
@@ -22,15 +29,15 @@ const TopicDetail = (props) => (
 )
 
 const homepage = (props) => {
-
   console.log(props);
-
   return (
     <div>
-      <Link to='/topics'>Topics</Link>
-      <h1>
-        HOME PAGE
-  </h1>
+      <h1>HOME PAGE </h1>
+
+      <Link to='/topics'>To Topics List Page</Link>
+       &nbsp;&nbsp;&nbsp;
+      <button onClick={() => props.history.push('/topics')}>To Topics List Page</button>
+
     </div>
   );
 }
@@ -40,12 +47,11 @@ const homepage = (props) => {
 // without exact all paths that match like / mathces with /homepage 
 // all such paths get rendered in the webpage
 
-
 function App() {
   return (
     <div>
 
-      <Route path='/' component={homepage} />
+      <Route exact path='/' component={homepage} />
       <Route exact path='/topics' component={TopicsList} />
       <Route path='/topics/:topicId' component={TopicDetail} />
 
@@ -58,7 +64,7 @@ function App() {
 // NOTE : Any component (like HomePage) gets rendered get passed 3 values
 // match 
 // history 
-// location
+// location -> gives us the path /absn/./.. where ever we are currently
 // do console.log(props) in that component to see these
 
 
